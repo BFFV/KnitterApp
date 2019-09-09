3696b7043e888eae4167d814a46cf5cbd2b4e1e0
narierao <nariera@uc.cl>
2019-09-09 15:49:54 -0300

    :ok_hand::sparkles: CRUD done!

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,14 @@ const KoaRouter = require('koa-router');
 const hello = require('./routes/hello');
 const index = require('./routes/index');
 const patterns = require('./routes/patterns');
+const categories = require('./routes/categories');
 
 const router = new KoaRouter();
 
 router.use('/', index.routes());
 router.use('/hello', hello.routes());
 router.use('/patterns', patterns.routes());
+router.use('/categories', categories.routes());
+
 
 module.exports = router;
