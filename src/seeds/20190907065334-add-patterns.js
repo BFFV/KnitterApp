5c90a9cefb36c41808fb5917c7a84f92a76226be
Benjamín Farías <bffarias@uc.cl>
2019-09-10 00:15:18 -0300

    :sparkles: :lipstick: :ok_hand: Added Materials & Patterns relation & some other improvements.

--- src/seeds/20190907065334-add-patterns.js
+++ src/seeds/20190907065334-add-patterns.js
@@ -6,6 +6,7 @@ module.exports = {
         instructions: '1) Dibujar círculos\n2) Dibujar cuadrados\n3) Repetir patrón cíclicamente',
         video: 'https://www.youtube.com/watch?v=1sg3Rt4KZpQ',
         image: 'imagen1',
+        tension: 'Leve',
         authorId: 1,
         categoryId: 1,
         createdAt: new Date(),
