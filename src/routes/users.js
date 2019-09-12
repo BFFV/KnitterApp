b2b8458503e693743f2c37f503c6f139b25e12d9
jjaguillon <jjaguillon@uc.cl>
2019-09-12 01:27:53 -0300

    Encrypt ready :lock:

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,13 @@
 const KoaRouter = require('koa-router');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const router = new KoaRouter();
 
+
 async function loadUser(ctx, next) {
   ctx.state.user = await ctx.orm.user.findByPk(ctx.params.id);
+  ctx.state.user.password = '';
   return next();
 }
 
@@ -39,7 +43,11 @@ router.get('users.edit', '/:id/edit', loadUser, async (ctx) => {
 
 router.post('users.create', '/', async (ctx) => {
   const user = ctx.orm.user.build(ctx.request.body);
+  
   try {
+    await bcrypt.hash(user.password, saltRounds).then(function(hash) {
+      user.password = hash;
+    });
     await user.save({ fields: ['username', 'password','email', 'age', 'photo', 'role'] });
     ctx.redirect(ctx.router.url('users.list'));
   } catch (validationError) {
@@ -54,9 +62,14 @@ router.post('users.create', '/', async (ctx) => {
 router.patch('users.update', '/:id', loadUser, async (ctx) => {
   const { user } = ctx.state;
   try {
-    const {
-      username, password, email, age, photo, role
-    } = ctx.request.body;
+    const username = ctx.request.body.username;
+    const email = ctx.request.body.email;
+    const age = ctx.request.body.age;
+    const photo = ctx.request.body.photo;
+    const role = ctx.request.body.role;
+    await bcrypt.hash(ctx.request.body.password, saltRounds).then(function(hash) {
+      password = hash;
+    });
     await user.update({
       username, password, email, age, photo, role
     });
