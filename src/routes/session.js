4cfe6f9042162d5028ad4fcf90526e3bf15fd1b6
BFFV <bffarias@uc.cl>
2019-10-18 01:20:27 -0300

    :sparkles: Added automatic role upgrade & some feedback stuff.

--- src/routes/session.js
+++ src/routes/session.js
@@ -2,6 +2,39 @@ const KoaRouter = require('koa-router');
 
 const router = new KoaRouter();
 
+// Starts the session & upgrades the user's role when certain conditions are met
+async function login(ctx, next) {
+  const status = await next();
+  const { user } = ctx.state;
+  if (status === 'logged') {
+    if (user.role === 'common') {
+      const { popular } = await ctx.orm.user.findOne({
+        raw: true,
+        attributes: [[ctx.orm.sequelize.fn('COUNT', ctx.orm.sequelize.col('username')), 'popular']],
+      });
+      const { relevant } = await ctx.orm.pattern.findOne({
+        raw: true,
+        where: {
+          authorId: user.id,
+        },
+        attributes: [[ctx.orm.sequelize.fn('AVG', ctx.orm.sequelize.col('popularity')), 'relevant']],
+      });
+      const { qualified } = await ctx.orm.pattern.findOne({
+        raw: true,
+        where: {
+          authorId: user.id,
+        },
+        attributes: [[ctx.orm.sequelize.fn('AVG', ctx.orm.sequelize.col('score')), 'qualified']],
+      });
+      if ((user.popularity >= popular * 0.2) && (relevant >= popular * 0.1) && (qualified >= 4)) {
+        await user.update({ role: 'top' });
+      }
+    }
+    ctx.redirect('/');
+  }
+  return 'Session Start';
+}
+
 router.get('session.new', '/new', (ctx) => ctx.render('session/new', {
   createSessionPath: ctx.router.url('session.create'),
   newUserPath: ctx.router.url('users.new'),
@@ -9,15 +42,16 @@ router.get('session.new', '/new', (ctx) => ctx.render('session/new', {
   notice: ctx.flashMessage.notice,
 }));
 
-router.put('session.create', '/', async (ctx) => {
+router.put('session.create', '/', login, async (ctx) => {
   const { email, password } = ctx.request.body;
   let error = 'Email o contraseña incorrectos!';
   try {
     const user = await ctx.orm.user.findOne({ where: { email } });
     const isPasswordCorrect = user && await user.checkPassword(password);
     if (isPasswordCorrect) {
+      ctx.state.user = user;
       ctx.session.token = user.token;
-      return ctx.redirect('/');
+      return 'logged';
     }
   } catch (e) {
     error = 'Parámetros NO válidos!';
