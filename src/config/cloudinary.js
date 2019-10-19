04fff24cea5f40043db2e1c443f1a0e5238b7528
jjaguillon <jjaguillon@uc.cl>
2019-10-19 18:18:18 -0300

    :pear: Added cloudinary config

---
+++ src/config/cloudinary.js
@@ -0,0 +1,5 @@
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.CLOUD_API, 
+  api_secret: process.env.CLOUD_API_SECRET
+});
--- src/routes/users.js
+++ src/routes/users.js
@@ -114,9 +114,12 @@ router.get('users.edit', '/:id/edit', loadUser, authenticate, async (ctx) => {
 
 router.post('users.create', '/', checkPassword, async (ctx) => {
   const params = ctx.request.body;
+  console.log(params);
   params.role = 'common';
   params.token = params.username;
   const user = ctx.orm.user.build(params);
+  const files = ctx.request.files;
+  console.log(files);
   try {
     await user.save({ fields: ['username', 'password', 'email', 'age', 'photo', 'role', 'token'] });
     ctx.redirect(ctx.router.url('session.new'));
