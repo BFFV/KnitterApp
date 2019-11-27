d710250bae0f8c17b210f3c83ac618b49185f212
BFFV <bffarias@uc.cl>
2019-11-27 16:33:26 -0300

    :ok_hand: Everything working.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,19 +3,18 @@ const KoaRouter = require('koa-router');
 const router = new KoaRouter();
 
 router.get('/', async (ctx) => {
-  // Fetch de API populares y valorados
-  
   const patterns = await ctx.orm.pattern.findAll();
   let favorites = [];
   if (ctx.state.currentUser) {
-  	const favoritesLoad = await ctx.state.currentUser.getFavoritePatterns();
-  	favorites = favoritesLoad.slice(0, 4);
+    const favoritesLoad = await ctx.state.currentUser.getFavoritePatterns();
+    favorites = favoritesLoad.slice(0, 4);
   }
   const patternsNewer = patterns.sort((a, b) => a.updatedAt - b.updatedAt).reverse().slice(0, 4);
-  const patternsPopular = patterns.sort((a, b) => a.popularity - b.popularity).reverse().slice(0, 4);
+  const patternsPopular = patterns.sort((a, b) => a.popularity - b.popularity)
+    .reverse().slice(0, 4);
   const patternsValues = patterns.sort((a, b) => a.score - b.score).reverse().slice(0, 4);
   await ctx.render('index', {
-  	favorites,
+    favorites,
     patternsNewer,
     patternsPopular,
     patternsValues,
